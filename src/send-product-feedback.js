ee3d7e0e92749af2aa3ce039372c84170263a114
Yousef Kama <yousef.kamawall@gmail.com>
2024-01-01 22:12:39 +0100

    add names to from and to in resend mail

--- src/send-product-feedback.js
+++ src/send-product-feedback.js
@@ -4,8 +4,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 module.exports = (product, message) => {
   return resend.emails.send({
-    from: 'product-feedback@resend.dev',
-    to: 'hello.dahalal@gmail.com',
+    from: 'Product Feedback Bot <product-feedback@resend.dev>',
+    to: 'DaHalal Team <hello.dahalal@gmail.com>',
     subject: `Produkt-Feedback erhalten: ${product.id}`,
     html: `<b>Produkt:</b> ${product.name || '<i>Noch nicht im System</i>'}<br><br><b>Nachricht:</b><br>${message.replace(/\\n/g, "<br>")}`,
   });
